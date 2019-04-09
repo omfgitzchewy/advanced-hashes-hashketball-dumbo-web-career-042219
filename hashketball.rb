36261632f052fec17500d95d2e70a071c7616ddd
Aaron Chu <aaronchu357@gmail.com>
2019-04-09 01:27:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -225,12 +225,11 @@ def big_shoe_rebounds
   max_player = nil
   game_hash.each do |location, team_data|
     team_data[:players].each do |player_name, player_stats|
-
-      if player_stats[:shoe] > max_player[:shoe] && (max_player || max_player = player_stats)
+      # max_player || max_player = player_stats
+      if player_stats[:shoe] > max_player[:shoe]
         max_player = player_stats
       end
     end
   end
-
   max_player[:rebounds]
 end
